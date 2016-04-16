16400473bcde58941e619346a3bee18926e45310
Todd Lucas <tl@toddlucas.net>
2016-04-16 16:15:57 -0700

    Rename EmptyFrame to AppFrame
    This naming is more consistent with React Router documentation.
    Add a header element in the App view and adjust other headers accordingly.

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -3,14 +3,13 @@ import * as React from 'react';
 import * as Router from 'react-router';
 import { Route, IndexRoute } from 'react-router';
 
-import EmptyFrame from './views/EmptyFrame';
+import AppFrame from './views/AppFrame';
 import NotFoundView from './views/NotFoundView';
 import HomeView from './views/HomeView';
 import AboutView from './views/AboutView';
 
-
 var routeMap = (
-    <Route path="/" component={EmptyFrame}>
+    <Route path="/" component={AppFrame}>
         <IndexRoute component={HomeView}/>
         <Route path="/about" component={AboutView}/>
         <Route path="*" component={NotFoundView} />
--- src/app/views/NotFoundView.tsx
+++ src/app/views/NotFoundView.tsx
@@ -4,8 +4,8 @@ import * as React from 'react';
 export default class NotFoundView extends React.Component<any, any> {
     render() {
         return <div>
-            <h1>404</h1>
-            <h2>Page not found</h2>
+            <h2>404</h2>
+            <h3>Page not found</h3>
         </div>;
     }
 }
